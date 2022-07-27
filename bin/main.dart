83bd4df67c620855651396ead82019fbde6f4e4e
Francisco Mendes <fmmagalhaesfilho@aluno.uespi.br>
2022-07-27 19:34:08 -0300

    Test One

--- bin/main.dart
+++ bin/main.dart
@@ -2,4 +2,7 @@ void main() {
     print('Teste');
     print('projeto SEGUNDA AVALIAÇÃO DE PROGRAMAÇÃO 2');
     print('Tiago da Silva Carvalho, Francisco Mendes Magalhães Filho, Edilson Morais Brito');
+    int a = 10;
+    int b = 20;
+    print(a+b);
 }
