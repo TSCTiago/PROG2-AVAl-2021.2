bc21cf6034426f7921f98aebcb924fcf19a5c6c3
Tiago Silva <87677785+TSCTiago@users.noreply.github.com>
2022-07-27 19:28:20 -0300

    commit teste

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,5 @@
 void main() {
+    print('Teste');
     print('projeto SEGUNDA AVALIAÇÃO DE PROGRAMAÇÃO 2');
     print('Tiago da Silva Carvalho, Francisco Mendes Magalhães Filho, Edilson Morais Brito');
 }
