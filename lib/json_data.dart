7b372612f95fc1c2d151d6c26a4a038d22332fa5
Tiago Silva <87677785+TSCTiago@users.noreply.github.com>
2022-07-29 16:48:27 -0300

    renemeando variáveis

--- lib/json_data.dart
+++ lib/json_data.dart
@@ -4,23 +4,20 @@ import 'dart:io';
 import 'data.dart';
 
 class JsonData implements Data {
-  dynamic data;
+  dynamic jsonData;
 
-  
   @override
   void load(jsonfile) {
     jsonfile = File(jsonfile).readAsStringSync();
-    data = jsonDecode(jsonfile);
-  
+    jsonData = jsonDecode(jsonfile);
   }
 
-
   @override
-  List<String> get fields => data[0].keys.toList();
+  List<String> get fields => jsonData[0].keys.toList();
   
 
   @override
-  bool get hasData => data.isEmpty;
+  bool get hasData => jsonData.isEmpty;
 
 
 }
