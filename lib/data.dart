5a7596ec4277ff0956ad7a6273f4b6b09a7eb609
Edilson Morais <moraisbritoedilson@gmail.com>
2022-07-29 18:04:13 -0300

    atualizando a main

--- lib/data.dart
+++ lib/data.dart
@@ -1,10 +1,19 @@
-abstract class Data{
+abstract class Data {
+  // Carrega o arquivo texto “fileName” para a memória.
   void load(String fileName);
-  List<String> get fields;
-  bool get hasData;
-  //void save;
-  String get data;
-  
-  //void clear;
 
+  // Grava os dados armazenados em memória no arquivo texto “fileName”.
+  void save(String fileName);
+
+  // Limpa os dados armazenados na memória interna do objeto.
+  void clear();
+
+  // Getter que indica se existem dados carregados na memória do objeto.
+  bool get hasData;
+
+  // Getter/setter que retorna/armazena dados na memória do objeto.
+  String get data;
+
+  // Getter que retorna os nomes dos campos de um registro.
+  List<String> get fields;
 }
