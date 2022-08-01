5e483903e13ad99f9767637596c3cb9e39df5b36
Tiago Silva <87677785+TSCTiago@users.noreply.github.com>
2022-07-31 22:39:21 -0300

    remove extension

--- bin/main.dart
+++ bin/main.dart
@@ -5,11 +5,10 @@ import 'package:prog2/json_data.dart';
 import 'package:prog2/xml_data.dart';
 
 void main() {
-
   //READ FILE JSON
-  print('-'*50);
+  print('-' * 50);
   print('                   JSON File Area');
-  print('-'*50);
+  print('-' * 50);
   JSONData jsonfile = JSONData();
   jsonfile.load('./files/example.json');
   print('Fields: ${jsonfile.fields}');
@@ -20,14 +19,13 @@ void main() {
   print('after clear\n');
   print('Has data: ${jsonfile.hasData}');
   print('Data: \n${jsonfile.data}');
-  print('='*50);
+  print('=' * 50);
   print('');
 
-
   //READ FILE XML
-  print('-'*50);
+  print('-' * 50);
   print('                   XML File Area');
-  print('-'*50);
+  print('-' * 50);
   XMLData xmlfile = XMLData();
   xmlfile.load('./files/example.xml');
   print('Fields: ${xmlfile.fields}');
@@ -38,13 +36,13 @@ void main() {
   print('after clear\n');
   print('Has data: ${xmlfile.hasData}');
   print('Data: \n${xmlfile.data}');
-  print('='*50);
+  print('=' * 50);
   print('');
 
   //READ FILE CSV
-  print('-'*50);
+  print('-' * 50);
   print('                   CSV File Area');
-  print('-'*50);
+  print('-' * 50);
   CSVData csvfile = CSVData();
   csvfile.load('./files/example.csv');
   print('Separator: ${csvfile.separator}');
@@ -56,14 +54,13 @@ void main() {
   print('after clear\n');
   print('Has data: ${csvfile.hasData}');
   print('Data: \n${csvfile.data}');
-  print('='*50);
+  print('=' * 50);
   print('');
 
-
   //READ FILE TSV
-  print('-'*50);
+  print('-' * 50);
   print('                   TSV File Area');
-  print('-'*50);
+  print('-' * 50);
   TSVData tsvfile = TSVData();
   tsvfile.load('./files/example.tsv');
   print('Separator: ${tsvfile.separator}');
@@ -75,8 +72,9 @@ void main() {
   print('after clear\n');
   print('Has data: ${tsvfile.hasData}');
   print('Data: \n${tsvfile.data}');
-  print('='*50);
+  print('=' * 50);
   print('');
 
-  print("Integrantes do Grupo: Edilson Morais Brito, Francisco Mendes Magalhães Filho, Tiago da Silva Carvaho");
+  print(
+      "Integrantes do Grupo: Edilson Morais Brito, Francisco Mendes Magalhães Filho, Tiago da Silva Carvaho");
 }
